286657c41005064e4027eafe34914759df1df6b4
Jackhaal <ejscloud@gmail.com>
2020-12-18 05:33:29 +0100

    version 3.4

--- src/main.js
+++ src/main.js
@@ -2,9 +2,11 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router/index'
 import Product from './components/Product.vue'
-
+import axios from 'axios'
+import VueAxios from 'vue-axios'
 
 const app = createApp(App)
 app.component("Product",Product)
 app.use(router)
 app.mount('#app')
+app.use(VueAxios, axios)
--- src/views/Where.vue
+++ src/views/Where.vue
@@ -6,13 +6,30 @@
     <img src="https://w3schools.com/w3images/map.jpg" class="w3-image" style="width:100%">
     <p><span class="w3-tag">FYI!</span> We offer full-service catering for any event, large or small. We understand your needs and we will cater the food to satisfy the biggerst criteria of them all, both look and taste.</p>
     <p><strong>Reserve</strong> a table, ask for today's special or just send us a message:</p>
-    <form @submit="checkForm" action="#" method="get">
+    <form @submit="checkForm" action="/where" method="get">
       <p><input class="w3-input w3-padding-16 w3-border" type="text" placeholder="Name" required name="Name"></p>
       <p><input class="w3-input w3-padding-16 w3-border" type="number" placeholder="How many people" required name="People"></p>
       <p><input class="w3-input w3-padding-16 w3-border" type="datetime-local" placeholder="Date and time" required name="date" value="2020-11-16T20:00"></p>
       <p><input class="w3-input w3-padding-16 w3-border" type="text" placeholder="Message \ Special requirements" required name="Message"></p>
-      <p><button class="w3-button w3-black" type="submit">SEND MESSAGE</button></p>
+      <p><button class="w3-button w3-black" type="submit">SEND MESSAGE</button> </p>
     </form>
+
+    <table v-show="currentUrl.Name" class="w3-table w3-white">
+                    <tr>
+                        <th>Name</th>
+                        <th>How many people</th>
+                        <th>Date</th>
+                        <th>Message</th>
+
+                    </tr>
+                    <tr>
+                        <td>{{currentUrl.Name}}</td>
+                        <td>{{currentUrl.People}}</td>
+                        <td>{{currentUrl.date}}</td>
+                        <td>{{currentUrl.Message}}</td>
+                    </tr>
+                  
+                </table>
   </div>
 </div>
 
@@ -22,18 +39,22 @@
 
 export default {
     name: 'Where',
-   
+    info: null,
         data(){
         return{
+            currentUrl : this.$route.query
+
 
         }
     },
-    methods:{
+   created: function(){
 
-    },
-    computed:{
+        
 
-    }
+      },
+      methods:{
+
+      }
 }
 </script>
 
